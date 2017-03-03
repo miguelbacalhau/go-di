323d5ed54f972eb2533415cc4be86e62585c3cdf
miguelbacalhau <miguelnjacinto@gmail.com>
2017-03-03 21:06:39 +0000

    change Service Interface to interface{}

--- DependencyInjection.go
+++ DependencyInjection.go
@@ -8,14 +8,14 @@ type ServiceInterface interface {
 
 // Standard Container interface
 type ContainerInterface interface {
-	Get(string) ServiceInterface
-	Has(string) bool
+	Get(service string) interface{}
+	Has(service string) bool
 }
 
 // Service Factory responsible for the Service instantiation.
 // All Service Factories must implement this interface
 type ServiceFactoryInterface interface {
-	CreateService(container ContainerInterface) ServiceInterface
+	CreateService(container ContainerInterface) interface{}
 }
 
 // Simple implementation of the container interface using
@@ -27,7 +27,7 @@ type Container struct {
 // Container Get Method implementation, returns the instance
 // of the Service registered in the Container with the name
 // provided.
-func (container *Container) Get(name string) ServiceInterface {
+func (container *Container) Get(name string) interface{} {
 	// TODO not found error instead of failing
 	return container.Services[name].CreateService(container)
 }
