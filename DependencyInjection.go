bbe26ac1c6349d07d115652b0b92397d1c140c48
miguelbacalhau <miguelnjacinto@gmail.com>
2017-03-20 21:13:22 +0000

    add delegate container feature

--- DependencyInjection.go
+++ DependencyInjection.go
@@ -21,22 +21,45 @@ type ServiceFactoryInterface interface {
 // Simple implementation of the container interface using
 // a map of string to Service Factories
 type Container struct {
-	Services map[string]ServiceFactoryInterface
+	services          map[string]ServiceFactoryInterface
+	delegateContainer ContainerInterface
 }
 
 // Container Get Method implementation, returns the instance
 // of the Service registered in the Container with the name
 // provided.
 func (container *Container) Get(name string) interface{} {
+	var dependencyContainer ContainerInterface
+
+	if container.delegateContainer != nil {
+		dependencyContainer = container.delegateContainer
+	} else {
+		dependencyContainer = container
+	}
 	// TODO not found error instead of failing
-	return container.Services[name].CreateService(container)
+	return container.services[name].CreateService(dependencyContainer)
 }
 
 // Container Has Method implementation, verifies if the Service
 // is registered in the Container with the name
 // provided
 func (container *Container) Has(name string) bool {
-	_, found := container.Services[name]
+	_, found := container.services[name]
 
 	return found
+}
+
+// Container Add method adds a new service entry mapped
+// to the given name
+func (container *Container) AddService(name string, service ServiceFactoryInterface) {
+	container.services[name] = service
+}
+
+// Container constructor, creates a new Container instance
+// if not needed then the delegate container should nil
+func NewContainer(services map[string]ServiceFactoryInterface, delegateContainer ContainerInterface) *Container {
+	return &Container{
+		services:          services,
+		delegateContainer: delegateContainer,
+	}
 }
